befc28bea1dc3f0c9359a87812aa06673be2e298
GarrySh <sooo.lame@gmail.com>
2018-11-14 18:00:51 +1000

    selection sort in progress

--- src/2.selectionSort.js
+++ src/2.selectionSort.js
@@ -1,1 +1,16 @@
-export default arr => arr;
+const getMinValueIndex = arr => {
+  let currentMinIndex = 0;
+  let currentMin = arr[currentMinIndex];
+  for (let i = 0; i <= arr.length; i += 1) {
+    if (arr[i] < currentMin) {
+      currentMin = arr[i];
+      currentMinIndex = i;
+    }
+  }
+  return currentMinIndex;
+};
+
+export default arr => {
+  const sortedArr = [];
+  return sortedArr;
+};
